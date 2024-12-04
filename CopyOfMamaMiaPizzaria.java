7612fc84974881f9fc14e30f66b4b49c33a014cf
BrennenderMartin <mattis2612@outlook.de>
2024-12-04 13:22:04 +0100

    Depri Edit nach Mathe

--- CopyOfMamaMiaPizzaria.java
+++ CopyOfMamaMiaPizzaria.java
@@ -22,26 +22,47 @@ public class CopyOfMamaMiaPizzaria
     String lieferadresse;
     
     int bestellung;
-       
+    
     // Konstruktor
     public CopyOfMamaMiaPizzaria()
     {
         fenster = new CopyOfDisplay("Pizzeria Traditore", this);
+        fenster();
+        fill_list();
+        
+        start();
+    }
+
+    // Dienste
+    public static void main(String[] args)
+    { 
+        @SuppressWarnings("unused")
+        CopyOfMamaMiaPizzaria main = new CopyOfMamaMiaPizzaria();
+    }
+
+    public void fenster()
+    {
         fenster.prompt("Name", "HungrigerTörke da Firenze");
         fenster.prompt("Bestellung", "Deportare Zaino"); // Abschieberucksack
         fenster.prompt("Lieferadresse", "Florenz");
-        
+    }
+
+    public void fill_list()
+    {
         // Alle Objekte der Speisekarte
         objectList.add(new MyObject("Deportare Zaino", 69.69));
         objectList.add(new MyObject("Pizza", 12.90));
         objectList.add(new MyObject("Pasta", 8.90));
         objectList.add(new MyObject("Pommes", 3.90));
-        
+    }
+    
+    public void start()
+    {
         bestellung = 0;
         while(bestellung < 10)
         {
             bestellung++;
-            fenster.ready("Taste \"Bestellen\" drücken, wenn alle Werte eingeben.");
+            fenster.ready("Taste \"Bestellen\" drücken, wenn alle Werte eingeben. \n");
             anschrift_name = fenster.getString("Name");
             bestellungsname = fenster.getString("Bestellung");
             lieferadresse = fenster.getString("Lieferadresse");
@@ -51,66 +72,50 @@ public class CopyOfMamaMiaPizzaria
             exe();
         }
     }
-    
-    public static void main(String[] args)
-    { 
-        CopyOfMamaMiaPizzaria object = new CopyOfMamaMiaPizzaria();
-        System.out.println(object);
-    }
-    
-    // Dienste
+
     public void exe()
     {
         fenster.println("Auftrag \"" + bestellungsname + 
                         "\" für \"" + anschrift_name + 
                         "\" in Bearbeitung nach \"" + lieferadresse + 
-                        "\".");
+                        "\". \n");
         
         if (objectList.contains(inputObject)) {
-            if(bestellungsname.equals("Deportare Zaino")){
-            
+            if(bestellungsname.equals("Deportare Zaino"))
+            {
                 fenster.println("Du kleiner Törke du! Tss tss tss...");
-                fenster.println("Zu faul etwas einzugeben? Tss tss tss...");
+                fenster.println("Zu faul etwas einzugeben? Tss tss tss...\n");
             
             } else { // Lässt nun das essen kochen
                 
-                fenster.println("Wird zubereitet...");
+                fenster.println("Wird zubereitet...\n");
                 
                 kochen(bestellungsname);
                 
             }
-        } else { // Error Meldung 
-            
-            fenster.println("Tippfehler oder nicht auf der Karte, bitte neu bestellen");
-            
-        }
+        } else {fenster.println("Tippfehler oder nicht auf der Karte, bitte neu bestellen\n");}
     }
     
     public void kochen(String bestellungsname)
     {
         fenster.println("Am kochen (" + bestellungsname + ") ...");
         // An Koch weitergeben
-        fenster.println("Das macht 35 € bitte! ");
-        bestellung--;
+        fenster.println("Das macht 35 € bitte! \n");
     }
     
     public void speisekarte()
     {
         fenster.println("Speisekarte:");
         int index = 1;
-        for(MyObject myobject : objectList)
-        {
-            fenster.println(index + 
-                            ". " + myobject.getName() + 
-                            ", Preis: " + myobject.getAttribute());
-        }
-        // Gibt die Speisekarte im Display aus
+        for(MyObject myobject : objectList){fenster.println(index + ". " + myobject.getName() + ", Preis: " + myobject.getAttribute());}
+        fenster.println(" ");
     }
     
     public void bezahlen()
     {
+        bestellung--;
         fenster.println("Vielen Dank für Ihren Einkauf!");
-        fenster.println("Der Bezahlvorgang ist abgeschlossen.");
+        fenster.println("Der Bezahlvorgang ist abgeschlossen. \n");
     }
 
     @Override
