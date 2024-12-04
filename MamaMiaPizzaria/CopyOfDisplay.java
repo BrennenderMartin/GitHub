2f5cd807dc0a852e424c2473b2196246b17bd91f
BrennenderMartin <mattis2612@outlook.de>
2024-12-04 20:08:21 +0100

    Move into oly Java Folder

--- MamaMiaPizzaria/CopyOfDisplay.java
+++ MamaMiaPizzaria/CopyOfDisplay.java
@@ -1,3 +1,4 @@
+package MamaMiaPizzaria;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.TextField;
--- MamaMiaPizzaria/CopyOfMamaMiaPizzaria.java
+++ MamaMiaPizzaria/CopyOfMamaMiaPizzaria.java
@@ -1,3 +1,4 @@
+package MamaMiaPizzaria;
 import java.util.ArrayList;
 import java.util.List;
 /**
