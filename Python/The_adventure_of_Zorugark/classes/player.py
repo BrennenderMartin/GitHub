04604b0117b16f7ccf622b07d8988e57d9ddf3c6
BrennenderMartin <mattis2612@outlook.de>
2024-12-31 23:22:39 +0100

    diese idee die random irgendwo auftaucht, um das zu fixen, aber nur dass es nicht funktioniert

--- Python/The_adventure_of_Zorugark/classes/player.py
+++ Python/The_adventure_of_Zorugark/classes/player.py
@@ -16,26 +16,34 @@ class Player(pg.sprite.Sprite):
     def update(self, keys, bg_offset_x, bg_offset_y):
         x = 0
         y = 0
+        
+        if pg.sprite.spritecollide(v.player, v.obstacles, False):
+            print("test")
+        
         if keys[pg.K_a]:
-            self.rect.x -= p.player_speed
+            if not pg.sprite.spritecollide(v.player, v.obstacles, False):
+                self.rect.x -= p.player_speed
             if self.rect.x < p.SCREEN_WIDTH // 2 and bg_offset_x > 0:
                 self.rect.x = p.SCREEN_WIDTH // 2
                 x -= p.player_speed
         
         if keys[pg.K_d]:
-            self.rect.x += p.player_speed
+            if not pg.sprite.spritecollide(v.player, v.obstacles, False):
+                self.rect.x += p.player_speed
             if self.rect.x > p.SCREEN_WIDTH // 2 and bg_offset_x < v.bg_width - p.SCREEN_WIDTH:
                 self.rect.x = p.SCREEN_WIDTH // 2
                 x += p.player_speed
         
         if keys[pg.K_w]:
-            self.rect.y -= p.player_speed
+            if not pg.sprite.spritecollide(v.player, v.obstacles, False):
+                self.rect.y -= p.player_speed
             if self.rect.y < p.SCREEN_HEIGHT // 2 and bg_offset_y > 0:
                 self.rect.y = p.SCREEN_HEIGHT // 2
                 y -= p.player_speed
         
         if keys[pg.K_s]:
-            self.rect.y += p.player_speed
+            if not pg.sprite.spritecollide(v.player, v.obstacles, False):
+                self.rect.y += p.player_speed
             if self.rect.y > p.SCREEN_HEIGHT // 2 and bg_offset_y < v.bg_height - p.SCREEN_HEIGHT:
                 self.rect.y = p.SCREEN_HEIGHT // 2
                 y += p.player_speed
