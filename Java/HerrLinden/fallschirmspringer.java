c87e0bea30e30de7a66f6f82d92b4dcbcff54f60
BrennenderMartin <mattis2612@outlook.de>
2025-03-14 14:01:50 +0100

    Update Fallschirmspringer Unterricht 14.03.2025

--- Java/HerrLinden/fallschirmspringer.java
+++ Java/HerrLinden/fallschirmspringer.java
@@ -14,6 +14,8 @@ public class fallschirmspringer {
 
         int n = 30; /* Anzahl Wiederholungen */
 
+        double[][] result = new double[n][4];
+
         for (int i = 0; i < n; i++) {
             t = t + delta_t;
             a = -(D / m) * x;
@@ -21,6 +23,10 @@ public class fallschirmspringer {
             v = v + a * delta_t;
             x = x + (v + v_alt) / 2 * delta_t;
             System.out.printf("t: %.2f a: %.3f v: %.3f h: %.3f%n", t, a, v, x);
+            result[i][0] = t;
+            result[i][1] = a;
+            result[i][2] = v;
+            result[i][3] = x; 
         }
     }
 }
